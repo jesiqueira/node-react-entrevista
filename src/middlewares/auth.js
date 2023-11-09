0a71e35c04365f6371c2c30309cb0879795a7548
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-11-08 21:05:57 -0300

    contruindo estrutura para middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import promisify from 'util';
+
+import authConfing from '../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token não é válido!' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decode = await promisify(jwt.verify(token), authConfing.secret);
+    req.userId = decode.id;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token não é válido' });
+  }
+
+  // const authenticated = true;
+
+  // if (authenticated) {
+  //   return next();
+  // } else {
+  //   return res.status(401).json();
+  // }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import HelloController from './controllers/HelloController';
 import UsersController from './controllers/UsersController';
+import RepositoriesController from './controllers/RepositoriesController';
+import auth from './middlewares/auth';
 
 const routes = new Router();
 
 routes.get('/hello', HelloController.index);
+
+routes.use(auth);
 
 //RESTFULL
 routes.get('/users', UsersController.index);
@@ -12,5 +16,9 @@ routes.get('/users/:id', UsersController.show);
 routes.post('/users', UsersController.create);
 routes.put('/users/:id', UsersController.update);
 routes.delete('/users/:id', UsersController.destroy);
+
+routes.get('/users/:user_id/repositories', RepositoriesController.index);
+routes.post('/users/:user_id/repositories', RepositoriesController.create);
+routes.delete('/users/:user_id/repositories', RepositoriesController.destroy);
 
 export default routes;
